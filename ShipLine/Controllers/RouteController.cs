5f595522b1128c0a4f11cec42f7cb833b4fb6932
bogse <ciucuras.bogdan@gmail.com>
2022-10-30 05:27:02 +0200

    Add Route controller functionality and views

--- ShipLine/Controllers/RouteController.cs
+++ ShipLine/Controllers/RouteController.cs
@@ -1,26 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShipLine.Data;
+using ShipLine.Models;
+using ShipLine.Repository;
 
 namespace ShipLine.Controllers
 {
     public class RouteController : Controller
     {
+        private RouteRepository _routeRepository;
+        public RouteController(ApplicationDbContext dbContext)
+        {
+            _routeRepository = new RouteRepository(dbContext);
+        }
+
         // GET: RouteController
         public ActionResult Index()
         {
-            return View();
+            var list = _routeRepository.GetAllRoutes();
+            return View(list);
         }
 
         // GET: RouteController/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(Guid id)
         {
-            return View();
+            var model = _routeRepository.GetRouteModelById(id);
+            return View("DetailsRoute", model);
         }
 
         // GET: RouteController/Create
         public ActionResult Create()
         {
-            return View();
+            return View("CreateRoute");
         }
 
         // POST: RouteController/Create
@@ -30,53 +41,70 @@ namespace ShipLine.Controllers
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var model = new RouteModel();
+                var task = TryUpdateModelAsync(model);
+                task.Wait();
+                if (task.Result)
+                {
+                    _routeRepository.InsertRoute(model);
+                }
+                return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View("CreateRoute");
             }
         }
 
         // GET: RouteController/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(Guid id)
         {
-            return View();
+            var model = _routeRepository.GetRouteModelById(id);
+            return View("EditRoute", model);
         }
 
         // POST: RouteController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Guid id, IFormCollection collection)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var model = new RouteModel();
+                var task = TryUpdateModelAsync(model);
+                task.Wait();
+                if (task.Result)
+                {
+                    _routeRepository.UpdateRoute(model);
+                }
+                return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return RedirectToAction("Edit", id);
             }
         }
 
         // GET: RouteController/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(Guid id)
         {
-            return View();
+            var model = _routeRepository.GetRouteModelById(id);
+            return View("DeleteRoute", model);
         }
 
         // POST: RouteController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(Guid id, IFormCollection collection)
         {
             try
             {
+                _routeRepository.DeleteRoute(id);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return RedirectToAction("Delete", id);
             }
         }
     }
