2b8273b54173d53087de600079805f5db2aeaa37
bogse <ciucuras.bogdan@gmail.com>
2022-11-12 10:48:00 +0200

    Fix Edit View for Shipments with complete fields. Add Date validation for Voyage Controller on  AddShipment method

--- ShipLine/Controllers/VoyageController.cs
+++ ShipLine/Controllers/VoyageController.cs
@@ -198,7 +198,8 @@ namespace ShipLine.Controllers
         {
             var model = _voyageRepository.GetVoyageById(id);
             var route = _routeRepository.GetRouteById(model.RouteId);
-            var shipments = _shipmentRepository.GetAllShipments().Where(x => x.DestinationPortId == route.DestinationPortId && x.SourcePortId == route.SourcePortId);
+            var shipments = _shipmentRepository.GetAllShipments().Where(x => x.DestinationPortId == route.DestinationPortId && x.SourcePortId == route.SourcePortId
+                            && x.NeedByDate > model.EndDate && model.StartDate > DateTime.Now);
 
             var shipmentList = shipments.Select(x => new SelectListItem(x.ShipmentNumber.ToString(), x.ShipmentId.ToString()));
             ViewBag.ShipmentList = shipmentList;
